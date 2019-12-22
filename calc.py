84d99dc627e5ed9d9a036f87ee8a913a7498327d
Crabtreek <wliersa15@gmail.com>
2019-12-22 01:47:46 +0000

    Returns inputted type(s)

--- calc.py
+++ calc.py
@@ -1,1 +1,89 @@
-print("this might work"
+normD=1
+fireD=1
+fightD=1
+watD=1
+flyD=1
+graD=1
+poiD=1
+eleD=1
+groundD=1
+psyD=1
+rockD=1
+iceD=1
+bugD=1
+draD=1
+ghostD=1
+darkD=1
+steelD=1
+fairyD=1
+type1= input("what pokemon type do you want to know about?").lower()
+type2= input("what is the secondary type? If none, say none").lower()
+
+
+def norm():
+  print("normal")
+def fire():
+  print("fire")
+def fight():
+  print("fighting")
+def wat():
+  print("water")
+def fly():
+  print("flying")
+def gra():
+  print("grass")
+def poi():
+  print("poison")
+def ele():
+  print("electric")
+def ground():
+  print("ground")
+def psy():
+  print("psychic")
+def rock():
+  print("rock")
+def ice():
+  print("ice")
+def bug():
+  print("bug")
+def dra():
+  print("dragon")
+def ghost():
+  print("ghost")
+def dark():
+  print("dark")
+def steel():
+  print("steel")
+def fairy():
+  print("fairy")
+def none():
+  pass
+typedefense={
+"normal": norm,
+"fire": fire,
+"fighting": fight,
+"fight": fight,
+"water": wat,
+"fly": fly,
+"flying": fly,
+"grass": gra,
+"poison": poi,
+"electric": ele,
+"ground": ground,
+"psychic": psy,
+"psy": psy,
+"rock": rock,
+"ice": ice,
+"bug": bug,
+"dragon": dra,
+"dra": dra,
+"ghost": ghost,
+"dark": dark,
+"steel": steel,
+"faerie": fairy,
+"fairy": fairy,
+"none": none
+}
+
+typedefense[type1]()
+typedefense[type2]()
