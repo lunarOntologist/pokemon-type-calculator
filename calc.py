61fa6f9793f0eb1e5c035580d23516bc3b01efd9
Crabtreek <wliersa15@gmail.com>
2019-12-23 23:25:35 +0000

    Defensive typing works for normal, fire, and fighting

--- calc.py
+++ calc.py
@@ -1,3 +1,12 @@
+
+global bugD 
+global fireD
+global graD
+global iceD
+global steelD
+global fightD
+global ghostD
+
 normD=1
 fireD=1
 fightD=1
@@ -16,16 +25,34 @@ ghostD=1
 darkD=1
 steelD=1
 fairyD=1
+
 type1= input("what pokemon type do you want to know about?").lower()
 type2= input("what is the secondary type? If none, say none").lower()
 
-
-def norm():
+def norm(vals):
   print("normal")
-def fire():
+  vals["fightD"] = vals["fightD"]*2
+  vals["ghostD"]=vals["ghostD"]*0
+  return vals
+
+def fire(vals):
   print("fire")
-def fight():
+  vals["bugD"] = vals["bugD"] * .5
+  vals["fireD"] = vals["fireD"]* .5
+  vals["graD"] = vals["graD"]* .5
+  vals["iceD"] = vals["iceD"]* .5
+  vals["steelD"] = vals["steelD"]* .5
+  vals["watD"] = vals["watD"]* 2
+  vals["groundD"] = vals["groundD"]* 2
+  vals["rockD"] = vals["rockD"]* 2
+  return vals
+def fight(vals):
   print("fighting")
+  vals["bugD"] = vals["bugD"]*.5
+  vals["rockD"] = vals["rockD"]*.5
+  vals["psyD"] = vals["psyD"]* 2 
+  vals["flyD"] = vals["flyD"]* 2
+  return vals
 def wat():
   print("water")
 def fly():
@@ -56,8 +83,8 @@ def steel():
   print("steel")
 def fairy():
   print("fairy")
-def none():
-  pass
+def none(vals):
+  return vals
 typedefense={
 "normal": norm,
 "fire": fire,
@@ -82,8 +109,24 @@ typedefense={
 "steel": steel,
 "faerie": fairy,
 "fairy": fairy,
-"none": none
+"none": none,
+"": none
 }
-
-typedefense[type1]()
-typedefense[type2]()
+vals = {
+  "bugD": 1,
+  "fireD": 1,
+  "graD": 1,
+  "iceD": 1,
+  "steelD": 1,
+  "rockD": 1,
+  "fightD": 1,
+  "groundD": 1,
+  "watD": 1,
+  "ghostD": 1,
+  "flyD": 1,
+  "psyD": 1,
+}
+vals = typedefense[type1](vals)
+vals = typedefense[type2](vals)
+print(vals)
+print(fireD)
