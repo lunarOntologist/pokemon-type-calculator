5a70d7f58b157d0416a80dbf2c17c3330766ba39
Crabtreek <wliersa15@gmail.com>
2020-01-05 07:01:58 +0000

    Defense against STAB moves is being worked on

--- calc.py
+++ calc.py
@@ -17,6 +17,18 @@ def norm(vals):
   print("normal")
   vals["fightD"] = vals["fightD"]*2
   vals["ghostD"]=vals["ghostD"]*0
+  if "steel" in vals:
+    vals["steel"] = vals["steel"] + "resists normal, "
+  else:
+    vals["steel"] = "resists normal, "
+  if "rock" in vals:
+    vals["rock"] = vals["rock"] + "resists normal, "
+  else:
+    vals["rock"] = "resists normal, "
+  if "ghost" in vals:
+    vals["ghost"] = vals["ghost"] + "immune normal, "
+  else:
+    vals["ghost"] = "immune to normal, "
   return vals
 def fire(vals):
   print("fire")
@@ -28,6 +40,39 @@ def fire(vals):
   vals["watD"] = vals["watD"]* 2
   vals["groundD"] = vals["groundD"]* 2
   vals["rockD"] = vals["rockD"]* 2
+  vals["fairyD"] = vals["fairyD"] * .5
+  if "bug" in vals:
+    vals["bug"] = vals["bug"] + "weak to fire, "
+  else:
+    vals["bug"] = "weak to fire, "
+  if "grass" in vals:
+    vals["grass"] = vals["grass"] + "weak to fire, "
+  else:
+    vals["grass"] = "weak to fire, "
+  if "ice" in vals:
+    vals["ice"] = vals["ice"] + "weak to fire, "
+  else:
+    vals["ice"] = "weak to fire, "
+  if "steel" in vals:
+    vals["steel"] = vals["steel"] + "weak to fire, "
+  else:
+    vals["steel"] = "weak to fire, "
+  if "rock" in vals:
+    vals["rock"] = vals["rock"] + "resists fire, "
+  else:
+    vals["rock"] = "resists fire, "
+  if "dragon" in vals:
+    vals["dragon"] = vals["dragon"] + "resists fire, "
+  else:
+    vals["dragon"] = "resists fire, "
+  if "water" in vals:
+    vals["water"] = vals["water"] + "resists fire, "
+  else:
+    vals["water"] = "resists fire, "
+  if "fire" in vals:
+    vals["fire"] = vals["fire"] + "resists fire, "
+  else:
+    vals["fire"] = "resists fire, "
   return vals
 def fight(vals):
   print("fighting")
@@ -36,15 +81,84 @@ def fight(vals):
   vals["psyD"] = vals["psyD"]* 2 
   vals["flyD"] = vals["flyD"]* 2
   vals["darkD"] = vals["darkD"]* 2
+  vals["fairyD"] = vals["fairyD"] * 2
+  if "poison" in vals:
+    vals["poison"] = vals["poison"] + "resists fighting, "
+  else:
+    vals["poison"] = "resists fighting, "
+  if "flying" in vals:
+    vals["flying"] = vals["flying"] + "resists fighting, "
+  else:
+    vals["flying"] = "resists fighting, "
+  if "bug" in vals:
+    vals["bug"] = vals["bug"] + "resists fighting, "
+  else:
+    vals["bug"] = "resists fighting, "
+  if "psychic" in vals:
+    vals["psychic"] = vals["psychic"] + "resists fighting, "
+  else:
+    vals["psychic"] = "resists fighting, "
+  if "fairy" in vals:
+    vals["fairy"] = vals["fairy"] + "weak to fighting, "
+  else:
+    vals["fairy"] = "resists fighting, "
+  if "ghost" in vals:
+    vals["ghost"] = vals["ghost"] + "immune to fighting, "
+  else:
+    vals["ghost"] = "immune to fighting, "
+  if "normal" in vals:
+    vals["normal"] = vals["normal"] + "weak to fighting, "
+  else:
+    vals["normal"] = "weak to fighting, "
+  if "steel" in vals:
+    vals["steel"] = vals["steel"] + "weak to fighting, "
+  else:
+    vals["steel"] = "weak to fighting, "
+  if "rock" in vals:
+    vals["rock"] = vals["rock"] + "weak to fighting, "
+  else:
+    vals["rock"] = "weak to fighting, "
+  if "ice" in vals:
+    vals["ice"] = vals["ice"] + "weak to fighting, "
+  else:
+    vals["ice"] = "weak to fighting, "
+  if "dark" in vals:
+    vals["dark"] = vals["dark"] + "weak to fighting, "
+  else:
+    vals["dark"] = "weak to fighting, "
   return vals
 def wat(vals):
   print("wat")
   vals["electricD"] = vals["electricD"]* 2
-  vals["grass"] = vals["grassD"]* 2
+  vals["grassD"] = vals["grassD"]* 2
   vals["fireD"] = vals["fireD"]* .5
   vals["iceD"] = vals["iceD"]* .5
   vals["steelD"] = vals["steelD"]* .5
   vals["watD"] = vals["watD"]* .5
+  if "fire" in vals:
+    vals["fire"] = vals["fire"] + "weak to water, "
+  else:
+    vals["fire"] = "weak to water, "
+  if "ground" in vals:
+    vals["ground"] = vals["ground"] + "weak to water, "
+  else:
+    vals["ground"] = "weak to water, "
+  if "rock" in vals:
+    vals["rock"] = vals["rock"] + "weak to water, "
+  else:
+    vals["rock"] = "weak to water, "
+  '''if "grass" in vals:
+    vals["grass"] = vals["grass"] + "resists water, "
+  else:
+    vals["grass"] = "resists water, "'''
+  if "dragon" in vals:
+    vals["dragon"] = vals["dragon"] + "resists water, "
+  else:
+    vals["dragon"] = "resists water, "
+  if "water" in vals:
+    vals["water"] = vals["water"] + "resists water, "
+  else:
+    vals["water"] = "resists water, "
   return vals
 def fly(vals):
   print("flying")
@@ -55,6 +169,30 @@ def fly(vals):
   vals["iceD"] = vals["iceD"]* 2
   vals["rockD"] = vals["rockD"]* 2
   vals["groundD"] = vals["groundD"]* 0
+  if "electric" in vals:
+    vals["electric"] = vals["electric"] + "resists flying, "
+  else:
+    vals["electric"] = "resists flying, "
+  if "steel" in vals:
+    vals["steel"] = vals["steel"] + "resists flying, "
+  else:
+    vals["steel"] = "resists flying, "
+  if "rock" in vals:
+    vals["rock"] = vals["rock"] + "resists flying, "
+  else:
+    vals["rock"] = "resists flying, "
+  if "bug" in vals:
+    vals["bug"] = vals["bug"] + "weak to flying, "
+  else:
+    vals["bug"] = "weak to flying, "
+  if "fighting" in vals:
+    vals["fighting"] = vals["fighting"] + "weak to flying, "
+  else:
+    vals["fighting"] = "weak to flying, "
+  if "grass" in vals:
+    vals["grass"] = vals["grass"] + "weak to flying, "
+  else:
+    vals["grass"] = "weak to flying, "
   return vals
 def grass(vals):
   print("grass")
@@ -67,6 +205,18 @@ def grass(vals):
   vals["fireD"] = vals["fireD"]* 2
   vals["iceD"] = vals["iceD"]* 2
   vals["poiD"] = vals["poiD"]* 2
+  if "water" in vals:
+    vals["water"] = vals["water"] + "weak to grass, "
+  else:
+    vals["water"] = "weak to grass, "
+  if "ground" in vals:
+    vals["ground"] = vals["ground"] + "weak to grass, "
+  else:
+    vals["ground"] = "weak to grass, "
+  if "rock" in vals:
+    vals["rock"] = vals["rock"] + "weak to grass, "
+  else:
+    vals["rock"] = "weak to grass, "
   return vals
 def poi(vals):
   print("poison")
@@ -155,6 +305,10 @@ def dark(vals):
   vals["darkD"] = vals["darkD"]* .5
   vals["ghostD"] = vals["ghostD"]* .5
   vals["psyD"] = vals["psyD"]* 0
+  if "ghost" in vals:
+    vals["ghost"] = vals["ghost"] + "resists"
+  else:
+    vals["ghost"] = "resists, "
   return vals
 def steel(vals):
   print("steel")
